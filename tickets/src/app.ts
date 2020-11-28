729b94112f91a5a803dca4af6d7eb86400df5cc8
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-11-28 15:22:21 -0500

    added tests and routes for another route handler in tickets service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import {createTicketRouter} from "./routes/new";
+import {showTicketRouter} from "./routes/show";
 import { errorHandler , NotFoundError , currentUser} from '@nayanika-test/common';
 
 const app = express();
@@ -17,6 +18,7 @@ app.use(
 
 app.use(currentUser)
 app.use(createTicketRouter)
+app.use(showTicketRouter)
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import {app} from "../../app";
+import {Ticket} from "../../models/ticket";
 
 it('has a route handler listening to /api/tickets for post requests',async() =>{
     const response =  await request(app)
@@ -26,11 +27,50 @@ it('returns a status other than 401 if user is signed in ',async() =>{
 })
 
 it('returns an error if an invalid title is provided',async() =>{
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: '',
+            price : 10
+        }).expect(400)
 
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+
+            price : 10
+        }).expect(400)
 })
 it('returns an error if an invalid price is provided',async() =>{
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: '',
+            price : -10
+        }).expect(400)
 
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'hjhkjk',
+        }).expect(400)
 })
 it('creates a ticket with valid parameters',async() =>{
+    //add in a check to make sure a ticket was saved
+    let tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(0)
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie',global.signin())
+        .send({
+            title: 'kjlkjlj',
+            price :20
+        }).expect(201)
 
+    tickets = await Ticket.find({})
+    expect(tickets.length).toEqual(1)
 })
