1eb926bd2d8c227c90efb6cf706617ac430bce42
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-12-16 18:54:35 -0500

    orders done

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import {app} from "../../app";
+import {Ticket} from "../../models/ticket";
+import {Order, OrderStatus} from "../../models/order";
+
+it('marks an order as cancelled',async() => {
+    // create a ticket with Ticket Model
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    })
+
+    await ticket.save();
+    //make a request to create an order
+    const user = global.signin();
+    const {body: order} = await request(app)
+        .post('/api/orders')
+        .set('Cookie',user)
+        .send({ticketId: ticket.id})
+        .expect(201);
+    // make a request to cancel the order
+    await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie',user)
+        .send()
+        .expect(204)
+
+    //expectation to make sure it is cancelled
+    const updatedOrder = await Order.findById(order.id)
+    expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
+})
