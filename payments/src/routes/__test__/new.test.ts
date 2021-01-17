16025f3b11e89739002ed761655782d0957efc5c
nayanika2304 <nayanikabhargava1993@gmail.com>
2021-01-17 12:54:41 -0500

    changes

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,70 +1,26 @@
-import request from "supertest";
-import {app} from '../../app';
-import mongoose from "mongoose";
-import {Order} from "../../models/order";
-import {OrderStatus} from "@nayanika-test/common";
-import {stripe} from "../../stripe";
-import{Payment} from "../../models/payment";
-
-jest.mock('../../stripe');
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { OrderStatus } from '@nayanika-test/common';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { stripe } from '../../stripe';
+import { Payment } from '../../models/payment';
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
     await request(app)
         .post('/api/payments')
-        .set('Cookie',global.signin())
+        .set('Cookie', global.signin())
         .send({
-            token : 'asdf',
-            orderId: mongoose.Types.ObjectId().toHexString()
+            token: 'asldkfj',
+            orderId: mongoose.Types.ObjectId().toHexString(),
         })
-        .expect(404)
+        .expect(404);
 });
 
-it('returns a 401 when purchasing an order that does not belong to the user', async () => {
+it('returns a 401 when purchasing an order that doesnt belong to the user', async () => {
     const order = Order.build({
         id: mongoose.Types.ObjectId().toHexString(),
         userId: mongoose.Types.ObjectId().toHexString(),
-        version: 0,
-        price: 20,
-        status: OrderStatus.Created
-    })
-
-    await order.save()
-    await request(app)
-        .post('/api/payments')
-        .set('Cookie',global.signin())
-        .send({
-            token : 'asdf',
-            orderId: order.id
-        })
-        .expect(401)
-});
-
-it('returns a 400 when purchasing a cancelled order', async () => {
-    const userId = mongoose.Types.ObjectId().toHexString();
-    const order = Order.build({
-        id: userId,
-        userId: mongoose.Types.ObjectId().toHexString(),
-        version: 0,
-        price: 20,
-        status: OrderStatus.Cancelled
-    })
-
-    await order.save()
-    await request(app)
-        .post('/api/payments')
-        .set('Cookie',global.signin(userId))
-        .send({
-            orderId: order.id,
-            token: 'jgjhjh'
-        })
-        .expect(400)
-});
-
-it('returns a 204 with valid inputs', async () => {
-    const userId = mongoose.Types.ObjectId().toHexString();
-    const order = Order.build({
-        id: mongoose.Types.ObjectId().toHexString(),
-        userId,
         version: 0,
         price: 20,
         status: OrderStatus.Created,
@@ -73,17 +29,33 @@ it('returns a 204 with valid inputs', async () => {
 
     await request(app)
         .post('/api/payments')
-        .set('Cookie', global.signin(userId))
+        .set('Cookie', global.signin())
         .send({
-            token: 'tok_visa',
+            token: 'asldkfj',
             orderId: order.id,
         })
-        .expect(201)
+        .expect(401);
+});
 
-    const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0]
-    expect(chargeOptions.source).toEqual('tok_visa')
-    expect(chargeOptions.amount).toEqual(20*100)
-    expect(chargeOptions.currency).toEqual('usd')
+it('returns a 400 when purchasing a cancelled order', async () => {
+    const userId = mongoose.Types.ObjectId().toHexString();
+    const order = Order.build({
+        id: mongoose.Types.ObjectId().toHexString(),
+        userId,
+        version: 0,
+        price: 20,
+        status: OrderStatus.Cancelled,
+    });
+    await order.save();
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', global.signin(userId))
+        .send({
+            orderId: order.id,
+            token: 'asdlkfj',
+        })
+        .expect(400);
 });
 
 it('returns a 201 with valid inputs', async () => {
@@ -114,10 +86,10 @@ it('returns a 201 with valid inputs', async () => {
 
     expect(stripeCharge).toBeDefined();
     expect(stripeCharge!.currency).toEqual('usd');
+
     const payment = await Payment.findOne({
         orderId: order.id,
-        stripeId: stripeCharge!.id
-    })
-    expect(payment).not.toBeNull()
+        stripeId: stripeCharge!.id,
+    });
+    expect(payment).not.toBeNull();
 });
-
